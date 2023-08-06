859eb8e2809225e91ea8634289ee5fba44b1d33b
TimoB90 <86852558+TimoB90@users.noreply.github.com>
2023-08-06 12:59:41 +0200

    Create smallestpPsitiveInteger.cpp

---
+++ smallestpPsitiveInteger/smallestpPsitiveInteger.cpp
@@ -0,0 +1,23 @@
+using System;
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+class Solution {
+    public int solution(int[] A) 
+    {
+        // Implement your solution here
+        int smallestNumber = 1;
+        Array.Sort(A);
+        for (int i = 0 ; i < A.Length; i++)
+        {
+            if (A[i] == smallestNumber)
+            {
+                smallestNumber++;
+            }
+        }
+        return smallestNumber;
+    }
+}
