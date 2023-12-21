659fc4c5aac161f816e16738e878a08cb34f492f
TimoB90 <86852558+TimoB90@users.noreply.github.com>
2023-12-21 12:08:16 +0100

    Update code.cpp

--- Domino Tiles/code.cpp
+++ Domino Tiles/code.cpp
@@ -1,1 +1,32 @@
+using System;
+using System.Collections.Generic;
 
+public class DominoTile
+{
+    public static void Main(string[] args)
+    {
+        string[] DominoTiles = {"0-0", "0-1", "1-2", "2-3"};
+        HashSet<string> TileSet = new HashSet<string>();
+        string Result = "";
+        
+        foreach (string i in DominoTiles) 
+        {
+            string reversedTile = i[2] + "-" + i[0];
+            TileSet.Add(i);
+            TileSet.Add(reversedTile);
+        } 
+        
+        for(int i=0; i<=6; i++)
+        {
+            for(int j=0; j<=6; j++)
+            {
+                string tmpTile = i + "-" + j;
+                if(!TileSet.Contains(tmpTile))
+                {
+                    Result += tmpTile + ",";
+                }
+            }
+        }
+        Console.WriteLine (Result);
+    }
+}
